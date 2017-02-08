98e0dbf9bdc2621709eb0a111a8358f5f0f20774
noah-springwater <nspringwater@gmail.com>
2017-02-08 17:23:51 -0500

    added port, env, ignore

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.load();
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT;
 
 const server = app.listen(8080, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
